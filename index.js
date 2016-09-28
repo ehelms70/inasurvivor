6f63c093c5effb2a6cc9f3ab274bfd9282728107
ehelms70 <ehelms70@gmail.com>
2016-09-28 19:28:48 -0400

    Initial commit to GIT... ever!

---
+++ index.js
@@ -0,0 +1,25 @@
+var Hapi = require('hapi');
+var $ = require('jquery');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!test');
+    }
+});
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
